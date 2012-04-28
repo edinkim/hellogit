920e99244efe0cceee2eaec67708b6b71f5ea0d2
edinkim <edin.0322@gmail.com>
2012-04-28 15:56:58 +0900

    test

---
+++ HelloTest/src/HelloTest.java
@@ -0,0 +1,12 @@
+
+public class HelloTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TEST ¿‘¥œ¥Ÿ");
+	}
+
+}
